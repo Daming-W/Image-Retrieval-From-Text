8108765cadb4f97261dd51cee83684cd86380cbe
Daming-W <83384577+Daming-W@users.noreply.github.com>
2023-05-20 00:34:25 +1000

    Update clip_ir.py

--- clip_ir.py
+++ clip_ir.py
@@ -19,7 +19,7 @@ from utils.preprocess import getImageTranform
 from utils.dataset import ImageNetDataset
 from utils.imagenet_utils import *
 from utils.evaluation_tools import *
-from dataset import get_datasets
+from dataset import voc, cifar
 
 ######################
 ### ArgumentParser ###
@@ -30,7 +30,7 @@ def parse_arguments():
     parser.add_argument("--eval_mode", type=bool, default=True, help="retrival on public data or not")
     # data
     parser.add_argument("--dataset_path", type=str, default="/root/autodl-tmp/clip_ir/cifar10/cifar10-batches", help="directory of dataset")
-    parser.add_argument("--dataset", type=str, default='voc', help="dataset name used for evaluation" )
+    parser.add_argument("--dataset", type=str, default='cifar', help="dataset name used for evaluation" )
     parser.add_argument("--num_class", type=int, default=1, help="total number of classes")      
     parser.add_argument("--save_dir", type=str, default='eval_result', help="directory to save the result")
     parser.add_argument("--resize", type=int, default=1, help="Resize image or not")
@@ -39,7 +39,7 @@ def parse_arguments():
     parser.add_argument("--gpu_id", type=int, default=0, help="GPU to work on")
     parser.add_argument("--batch_size", type=int, default=10, help='batch size which is the candidates size')
     parser.add_argument("--workers", type=int, default=0, help="workers")
-    parser.add_argument("--sim_th", type=float, default=5.5, help="threshold to determine retrieval")
+    parser.add_argument("--sim_threshold", type=float, default=5.5, help="threshold to determine retrieval")
     # model and cls
     parser.add_argument("--clip_model_name", type=str, default='RN50', help="visual Model name of CLIP")
     parser.add_argument("--sentence_prefix", type=str, default='word', help="input text type: \"sentence\", \"word\"")
@@ -49,64 +49,18 @@ def parse_arguments():
     parser.add_argument("--num_pca_components", type=int, default=100, help="pca target dimensions")
     return parser.parse_args()
 
-#########################
-### Data Loading Code ###
-#########################
-def load_data(dataset_path, file_name , preprocess):
-    """Load CIFAR-10 data from given dataset_path"""
-    file_path = os.path.join(dataset_path, file_name)
-    with open(file_path, 'rb') as f:
-        # load data with pickle
-        entry = pickle.load(f, encoding='latin1')
-        images_array = entry['data']
-        labels = entry['labels']
-        # reshape the array to 32x32 color image
-        images_array = images_array.reshape(-1, 3, 32, 32)  # '-1' means the value will be deduced from the shape of array
-        images =[]
-        # loop array by [0] which is the data idx
-        for i in range(images_array.shape[0]):
-            # array->img and do preprocess
-            image_array = images_array[i].transpose(1, 2, 0)
-            image = Image.fromarray(image_array)
-            image = preprocess(image.convert('RGB'))
-            images.append(image)
-        # images,labels : list of data
-    return images, labels
-
-class cifar10_dataset(Dataset):
-    def __init__(self, dataset_path,preprocess, kind='test'):
-        super().__init__()
-        # split train-valid-test if need
-        if kind == 'train':
-            self.file_list = ['data_batch_1', 'data_batch_2','data_batch_3','data_batch_4']
-        elif kind == 'valid':
-            self.file_list = ['data_batch_5']
-        else:
-            self.file_list = ['test_batch']
-        # create list for store data
-        self.images,self.labels = [],[]
-        # save list from load_data
-        for file_name in self.file_list:
-            file_path = os.path.join(dataset_path, file_name)
-            images, labels = load_data(dataset_path, file_name, preprocess)
-            self.images = images
-            self.labels = labels
-    def __len__(self):
-        return len(self.labels)
-    def __getitem__(self, idx):
-       image = self.images[idx]
-       label = self.labels[idx]
-       return image, label
 
 ###########################
 ### Retrieval Pred Func ###
 ###########################
 def retrieval_clip(args, image_query, text_tokens, clip_model):
     print(f'retrievalling from {len(image_query)} images...')
+
     # encode texts
     text_tokens = text_tokens.cuda(non_blocking=True)
     text_emb = clip_model.encode_text(text_tokens)
 
+    # run CLIP with evaluation model
     if args.eval_mode==True:
         # encode images
         image_query = image_query.cuda(non_blocking=True)
@@ -117,9 +71,10 @@ def retrieval_clip(args, image_query, text_tokens, clip_model):
         print(f'sim_score: {sim_score}')
         # when retrieve own images,return the most likely image
         # thus compute the softmax
-        bin_score = [1 if sim>=args.sim_th else 0 for sim in sim_score]
+        bin_score = [1 if sim>=args.sim_threshold else 0 for sim in sim_score]
         return bin_score,sim_score
 
+    # run CLIP for user testing
     if args.eval_mode==False:
         # set score list 
         sim_score = []
@@ -141,18 +96,45 @@ def retrieval_clip(args, image_query, text_tokens, clip_model):
 def evaluate_clip(args, labels_dict, dataloader, clip_model):
     # loop validation dataloader
     precision_list, recall_list, fscore_list = [],[],[]
+
     for i, (images,labels) in enumerate(dataloader):
-        # convert labels to set to get all retrivable label
-        labels_set={label.item() for label in labels}
-        # get one label from set to test
-        label_rand=random.choice(list(labels_set))
-        text = labels_dict[label_rand]
-        # load prompt and tokenize
-        print(f'the random text for IR is {text}')
-        prompt = "this is a photo of a "
-        text_tokens = clip_modified.tokenize(prompt+text)
-        # set ground truth 
-        ground_truth = [1 if label==label_rand else 0 for label in labels]
+        
+        # cifar label come with label-index
+        if args.dataset == 'cifar' or args.dataset == 'cifar10':
+            # convert labels to set to get all retrivable labels
+            labels_set={label.item() for label in labels}
+            # get one label from set to test
+            label_rand=random.choice(list(labels_set))
+            text = labels_dict[label_rand]
+            # load prompt and tokenize
+            print(f'the random text for IR is {text}')
+            prompt = "this is a photo of a "
+            text_tokens = clip_modified.tokenize(prompt+text)
+            # set ground truth 
+            ground_truth = [1 if label==label_rand else 0 for label in labels]
+
+        # voc labels come with one-hot type
+        elif args.dataset == 'voc' or args.dataset == 'voc2007':
+            # get union list for retrivable labels
+            union_list = [int(max(values).item()) for values in zip(*list(labels))]
+            union_label = [i for i, value in enumerate(union_list) if value == 1]
+            # get one label from set to test
+            label_rand = random.choice(union_label)
+            print(label_rand)
+            text = labels_dict[label_rand]
+            # load prompt and tokenize
+            print(f'the random text for IR is {text}')
+            prompt = "this is a photo of a "
+            text_tokens = clip_modified.tokenize(prompt+text)
+            # set ground truth
+            ground_truth = [1 if label[label_rand]==1 else 0 for label in labels]
+            print(f'labels{labels}') 
+            print(f'label_rand{label_rand}')
+            print(f'text{text}')
+            print(f'ground_truth{ground_truth}')
+
+            break
+
         # do clip-ir
         bin_score,sim_score = retrieval_clip(args, images, text_tokens, clip_model)
         precision, recall, fscore, support = precision_recall_fscore_support(ground_truth,bin_score)
@@ -171,23 +153,10 @@ if __name__ == '__main__':
     import warnings
     warnings.filterwarnings("ignore")
     np.seterr(divide='ignore',invalid='ignore')
-    # clear cuda cache and print key info
-    torch.cuda.empty_cache()
-    from pynvml import *
-    nvmlInit()
-    h = nvmlDeviceGetHandleByIndex(0)
-    info = nvmlDeviceGetMemoryInfo(h)
-    print(f'Total mem : {info.total}')
-    print(f'Free mem  : {info.free}')
-    print(f'Used mem  : {info.used}')
-
+    
     # get all args
     args = parse_arguments()
     print('parse arguments all set')
-
-    # create labels dict for cifar
-    labels_dict = {0: "airplane", 1: "automobile", 2: "bird", 3: "cat", 
-    4: "deer", 5: "dog", 6: "frog", 7: "horse", 8: "ship", 9: "truck"}
 
     # get model ==> Clip model
     clip_model,preprocess = clip_modified.load(args.clip_model_name, device = args.gpu_id, jit = False)
@@ -197,14 +166,52 @@ if __name__ == '__main__':
     # do evaluation
     if args.eval_mode==True:
         print('do evaluation')
+
         # get dataset and dataloader
-        cifar10 = cifar10_dataset(args.dataset_path,preprocess)
-        dataloader = DataLoader(cifar10,batch_size=args.batch_size,
-                                num_workers=args.workers,
-                                shuffle=True)
+        if args.dataset == 'voc' or args.dataset == 'voc2007':
+
+            args.dataset_path = '/root/autodl-tmp/clip_ir/VOC2007'
+            voc2017 = voc.Voc2007Classification(root=args.dataset_path, set='test', 
+                                                transform=preprocess)
+            dataloader = DataLoader(voc2017,
+                                    batch_size=args.batch_size,
+                                    num_workers=args.workers,
+                                    shuffle=True)
+            # create labels dict for cifar
+            labels_dict = {0: 'aeroplane', 1: 'bicycle', 2: 'bird', 3: 'boat', 
+            4: 'bottle', 5: 'bus', 6: 'car', 7: 'cat', 8: 'chair', 9: 'cow', 
+            10: 'dining table', 11: 'dog', 12: 'horse', 13: 'motorbike', 14: 'person', 
+            15: 'potted plant', 16: 'sheep', 17: 'sofa', 18: 'train', 19: 'tv monitor'}
+
+        elif args.dataset == 'cifar' or args.dataset == 'cifar10':
+
+            args.dataset_path = '/root/autodl-tmp/clip_ir/cifar10/cifar10-batches'
+            cifar10 = cifar.cifar10_dataset(args.dataset_path,preprocess)
+            dataloader = DataLoader(cifar10,
+                                    batch_size=args.batch_size,
+                                    num_workers=args.workers,
+                                    shuffle=True)
+            # create labels dict for cifar
+            labels_dict = {0: "airplane", 1: "automobile", 2: "bird", 3: "cat", 
+            4: "deer", 5: "dog", 6: "frog", 7: "horse", 8: "ship", 9: "truck"}
+        
+        elif args.dataset == 'caltech' or args.dataset == 'caltech101':
+            
+            args.dataset_path = '/root/autodl-nas/caltech-101/'
+            caltech = torchvision.datasets.Caltech101(root = args.dataset_path,
+                                                      transform = preprocess
+                                                      )
+            dataloader = DataLoader(caltech,
+                                    batch_size=args.batch_size,
+                                    num_workers=args.workers,
+                                    shuffle=True)
+
+        else: print('Dataset asked not found')
+
         print(f'total batches num : {len(dataloader)}')
         print(f'total img-cls num : {len(dataloader)*args.batch_size}')
         
+        # main evaluation func
         evaluate_clip(args, labels_dict, dataloader, clip_model)
 
     # retrieve for own image query
