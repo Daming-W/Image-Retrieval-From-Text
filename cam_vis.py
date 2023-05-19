f20bf3942a49035f0c2f1fde5620a5c012ccac43
Daming-W <83384577+Daming-W@users.noreply.github.com>
2023-05-20 00:02:02 +1000

    Update cam_vis.py

--- cam_vis.py
+++ cam_vis.py
@@ -1,163 +1,133 @@
 import clip_modified
 import torch
-import torchvision
-from PIL import Image, ImageDraw
+from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
-import os
-from matplotlib import patches as mtp_ptch
-from torchvision import transforms
-from tqdm.notebook import tqdm
 import argparse
-import cv2
-import json
-import heapq
-from sklearn.preprocessing import normalize
-from sklearn.metrics import average_precision_score
-import utils.aslloss 
+import os
+import time
+from utils.evaluation_tools import getHeatMap, getHeatMapNoBBox, getHeatMapOneBBox, MaskToBBox
 from utils.model import getCLIP, getCAM
 from utils.preprocess import getImageTranform
-from utils.dataset import ImageNetDataset
-from utils.imagenet_utils import *
-from utils.evaluation_tools import *
-from tqdm import tqdm
-from dataset.get_dataset import get_datasets
+from clip_modified import build_model
+import cv2
 
+RESIZE = 1
+
+ImageTransform = getImageTranform(resize=RESIZE)
+originalTransform = getImageTranform(resize=RESIZE, normalized=False)
+
+
+def clipcam(CLIP_MODEL_NAME, CAM_MODEL_NAME, images, sentence, DISTILL_NUM = 0, ATTACK = None, GPU_ID = 'cpu'):
+    
+    model, target_layer, reshape_transform = getCLIP(model_name=CLIP_MODEL_NAME, gpu_id=GPU_ID)
+    
+    cam = getCAM(model_name=CAM_MODEL_NAME, model=model, target_layer=target_layer, gpu_id=GPU_ID, reshape_transform=reshape_transform)
+    MASK_THRESHOLD = get_mask_threshold(CLIP_MODEL_NAME)
+    
+    final_img = get_clipcam_single(cam, model, MASK_THRESHOLD, images[0], sentence, DISTILL_NUM = DISTILL_NUM, ATTACK = ATTACK, GPU_ID = GPU_ID)
+
+    del cam
+    del model
+    return final_img
+
+# get mask for generating bbox if need
+def get_mask_threshold(CLIP_MODEL_NAME):
+    if CLIP_MODEL_NAME == 'RN50':
+        MASK_THRESHOLD = 0.2
+    if CLIP_MODEL_NAME == 'RN101':
+        MASK_THRESHOLD = 0.2
+    if CLIP_MODEL_NAME == 'ViT-B/16':
+        MASK_THRESHOLD = 0.3
+    if CLIP_MODEL_NAME == 'ViT-B/32':
+        MASK_THRESHOLD = 0.3
+    return MASK_THRESHOLD
+
+# compute heatmap over the original image
+def get_clipcam(clipcam, model, MASK_THRESHOLD, image, sentence = None, DISTILL_NUM = 0, ATTACK = None, GPU_ID = 'cpu'):
+    image = image
+    orig_image = image
+    image = ImageTransform(image)
+    orig_image = originalTransform(orig_image)
+    image = image.unsqueeze(0)
+    image = image.to(GPU_ID)
+    orig_image = orig_image.to(GPU_ID)
+
+    if sentence == None:
+        sentence = input(f"Please enter the query sentence: ")
+    text  = clip_modified.tokenize(sentence)
+    text = text.to(GPU_ID)
+    text_features = model.encode_text(text)
+    
+    logits_per_image,logits_per_text = model(image, text)
+    sim_score = float((logits_per_image+logits_per_text)/2)
+    print(f'sim score : {sim_score}')
+
+    grayscale_cam = clipcam(input_tensor=image, text_tensor=text_features)[0, :]
+    grayscale_cam_total = grayscale_cam[np.newaxis, :]
+
+    grayscale_cam_mask = np.where(grayscale_cam_total < MASK_THRESHOLD, 0, 1)
+    pred_bbox, pred_mask = MaskToBBox(grayscale_cam_mask, 1)
+    final_img = getHeatMapOneBBox(grayscale_cam, orig_image.permute(1, 2, 0).cpu().numpy(), pred_bbox, sentence)
+    #final_img = getHeatMapNoBBox(grayscale_cam,orig_image.permute(1, 2, 0).cpu().numpy(),'test')
+    return final_img
+
+# setup args
 parser = argparse.ArgumentParser()
-# data
-parser.add_argument("--dataset_dir", type=str, 
-                    default='/home/notebook/data/group/projects/tagging/caption/datasets/public/coco2014',
-                    help="directory of dataset")
-parser.add_argument("--dataset", type=str, default='coco', help="dataset name used for evaluation" )
-parser.add_argument("--num_class", type=int, default=1, help="total number of classes")      
-parser.add_argument("--save_dir", type=str, default='eval_result', help="directory to save the result")
-parser.add_argument("--resize", type=int, default=1, help="Resize image or not")
-
-# hyper parameters
-parser.add_argument("--gpu_id", type=int, default=0, help="GPU to work on")
-parser.add_argument("--batch_size", type=int, default=32, help="batch size")
-parser.add_argument("--workers", type=int, default=32, help="batch size")
-parser.add_argument("--epochs", type=int, default=20, help="the number of epochs")
-
-# model and cls
-parser.add_argument("--clip_model_name", type=str, default='RN101', help="Model name of CLIP")
-parser.add_argument("--sentence_prefix", type=str, default='word', help="input text type: \"sentence\", \"word\"")
-parser.add_argument("--sim2log", type=str, default='softmax', help="method to compute logits from similarity")
+parser.add_argument("--do_compare", type=bool, default =False,
+                    help="if true, show both clip orig and ft to compare")
+parser.add_argument("--image_folder_path", type=str, default='/root/autodl-tmp/clip_ir/image_candidates/',
+                    help="single image path or 4 images directory (grid)")                
+parser.add_argument("--image_name", type=str,
+                    help="single image path or 4 images directory (grid)")
+parser.add_argument("--gpu_id", type=str, default='cpu',
+                    help="GPU id to work on, \'cpu\'.")
+parser.add_argument("--clip_model_name", type=str,
+                    default='RN50', help="Model name of CLIP")
+parser.add_argument("--cam_model_name", type=str,
+                    default='GradCAM', help="Model name of GradCAM")
+parser.add_argument("--resize", type=int,
+                    default=1, help="Resize image or not")
+parser.add_argument("--distill_num", type=int, default=0,
+                    help="Number of iterative masking")
+parser.add_argument("--attack_type", type=str, default=None,
+                    help="attack type: \"snow\", \"fog\"")
+parser.add_argument("--sentence", type=str, default='',
+                    help="input text")
+parser.add_argument("--vis_res_folder_path", type=str, default='/root/autodl-tmp/clip_ir/cam_vis/',
+                    help="path of vis results")                  
 args = parser.parse_args()
 
-# Data loading code
-if args.dataset=='coco':
-    args.num_class = 80
-    args.dataset_dir = '/home/notebook/data/group/projects/tagging/caption/datasets/public/coco2014'
-elif args.dataset=='voc':
-    args.num_class = 20
-    args.dataset_dir = '/home/notebook/data/group/projects/tagging/caption/datasets/public/voc2007'
-elif args.dataset=='openimage':
-    args.num_class = 567
-    args.dataset_dir = '/home/notebook/data/group/projects/tagging/caption/datasets/public/open-images-v6'
+id = input(f'image id: ')
+args.image_name = str(id) + '.jpeg'
+args.sentence = str(input(f'sentence: '))
+
+# setup device
+if args.gpu_id != 'cpu':
+    args.gpu_id = int(args.gpu_id)
+# Image read target image
+image_path=args.image_folder_path + args.image_name
+if os.path.isfile(image_path):
+    img = Image.open(image_path)
+    images = [img]
 else:
-    NotImplementedError
+    images = []
+    for f in os.listdir(image_path):
+        images.append(Image.open(os.path.join(image_path, f)))
 
-label_list=[]
-if args.dataset == 'coco':
-    json_path = os.path.join(args.dataset_dir,'annotations/instances_val2014.json')
-    json_labels = json.load(open(json_path,"r"))
-    for i in json_labels["categories"]:
-        label_list.append(i['name'])
+        
+# get model and load weights 
+itc_model, target_layer, reshape_transform = getCLIP(model_name=args.clip_model_name, gpu_id=args.gpu_id)
+print('model loaded')
 
-elif args.dataset == 'voc':
-    label_list  = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 
-                    'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+# get CAM model based on itc model and cam method
+cam = getCAM(model_name=args.cam_model_name, 
+            model=itc_model, target_layer=target_layer, 
+            gpu_id=args.gpu_id, reshape_transform=reshape_transform)
 
-elif args.dataset == 'openimage':
-    label_list = []
+# compute cam
+final_img_1 = get_clipcam(cam, itc_model, get_mask_threshold(args.clip_model_name), 
+                            image=img, sentence=args.sentence, 
+                            DISTILL_NUM = args.distill_num, ATTACK = args.attack_type, GPU_ID = args.gpu_id)
 
-else:
-    raise NotImplementedError()
-
-train_dataset, val_dataset = get_datasets(args)
-
-train_loader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=args.batch_size, shuffle=True,
-    num_workers=args.workers, pin_memory=True, drop_last=True)
-
-val_loader = torch.utils.data.DataLoader(
-    val_dataset, batch_size=args.batch_size, shuffle=False,
-    num_workers=args.workers, pin_memory=True)
-
-sample = next(iter(val_loader))
-print(sample[0])
-print(sample[1])
-
-'''
-for i, img in enumerate(sample[0]):
-    if i==1:
-        img = img.detach().numpy()
-        print(img.shape)
-        print(type(img))
-        img = img.reshape(224,224,3)
-        plt.imshow(img)
-print(f"Label: {sample[1]}")
-'''
-
-# get target model
-class Resnet50(torch.nn.Module):
-    def __init__(self, num_class):
-        super().__init__()
-        resnet = torchvision.models.resnet50(pretrained=True)
-        resnet.fc = torch.nn.Sequential(torch.nn.Dropout(p=0.2),
-            torch.nn.Linear(in_features=resnet.fc.in_features, out_features=num_class))
-        self.base_model = resnet
-        self.Sigmoid = torch.nn.Sigmoid()
-    def forward(self, input):
-        logits = self.Sigmoid(self.base_model(input))
-        return logits
-
-RN50_model = Resnet50(args.num_class).cuda()
-
-target_layer = RN50_model.base_model.layer4[-1]
-RN50_model_cam = getCAM(model_name="GradCAM_original",model=RN50_model, 
-    target_layer=target_layer, gpu_id=args.gpu_id,reshape_transform=None)
-
-input = sample[0].cuda()
-heatmap = RN50_model_cam(input)
-print(heatmap.shape)
-
-heatmap_group=[]
-for index in range(80):
-    heatmap = RN50_model_cam(input,target_category=index)
-    heatmap_group.append(heatmap)
-print("heatmap group shape: ", np.asarray(heatmap_group).shape)
-
-# testing CLIP cam batch output
-tea_model, target_layer, reshape_transform, preprocess = getCLIP(
-        model_name="RN50", gpu_id=args.gpu_id)
-
-CLIP_model_cam = getCAM(model_name="GradCAM", model=tea_model, 
-                        target_layer=tea_model.visual.layer4[-1],
-                        gpu_id=args.gpu_id, reshape_transform=reshape_transform)
-                       
-input = sample[0].cuda()
-heatmap_group=[]
-text_features =[]
-for label in label_list:
-    text_tokens = clip_modified.tokenize("this is a photo of a "+label).cuda()
-    text_features.append(tea_model.encode_text(text_tokens))
-
-for text_feature in text_features:
-    heatmap = CLIP_model_cam(input,text_feature)
-    heatmap_group.append(heatmap)
-print("heatmap group shape: ", np.asarray(heatmap_group).shape)
-
-'''
-# testing CLIP cam batch output: Method2
-text_labels_p = [f"this is a photo of a {label}" for label in label_list]
-text_tokens = clip_modified.tokenize(text_labels_p).cuda()
-text_features = tea_model.encode_text(text_tokens)
-print(text_features.shape)
-print(text_features[0].shape)
-text_features[0] = text_features[0].expand(1,-1)
-print(text_features[0].shape)
-
-heatmap = CLIP_model_cam(input,text_features[0])
-'''
+final_img_1.save(args.vis_res_folder_path + f'{id}_{args.sentence}.png')
+print('visualization done')
